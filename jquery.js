c128de402314aa53264209ae0636d6a8b15494e4
Jay Blake <jayblake@Jays-MacBook-Air.local>
2015-09-14 00:32:02 -0500

    completed

--- jquery.js
+++ jquery.js
@@ -1,34 +1,3 @@
-/*var sentences = [
-	'ten ate neite ate nee enet ite ate inet ent eate',
-	'Too ato too nOt enot one totA not anot tOO aNot',
-	'oat itain oat tain nate eate tea anne inant nean',
-	'itant eate anot eat nato inate eat anot tain eat',
-	'nee ene ate ite tent tiet ent ine ene ete ene ate'
-];
-
-var words=0;
-var mistakes=0;
-var letterCount=0;
-var done = false;
-var newLineStarted = true;
-var currentLetter;
-var lines = [];
-var lineCount = 0;
-
-$(sentences).each(function(){
-   var letters = this.split('');
-   console.log(letters);
-   var line = $('<div class="letters"></div>');
-   $(letters).each(function() {
-       var letter = this;
-       if(letter == ' ') {
-           letter = '&nbsp;';
-       }
-       line.append($('<div class="letter"></div>').html(letter));
-   });
-   lines.push(line);
-});*/
-
 $(document).ready(function(){
 	var sentences = [
 		'ten ate neite ate nee enet ite ate inet ent eate',
@@ -38,12 +7,22 @@ $(document).ready(function(){
 		'nee ene ate ite tent tiet ent ine ene ete ene ate'
 	];
 
-	var words=0;
-	var mistakes=0;
-	var letterCount=0;
-	var done = false;
+	var typedWords = 0;
+	var letterCount = 0;
+	var mistakeCount = 0;
+	var bStarted = false;
+	var bDone = false;
+	var bCharPressedLast = false;
+	var dte = new Date();
+	var start_time;
+	var stop_time;
 	var newLineStarted = true;
+	var key_char;
+	var key_id;
 	var currentLetter;
+	var currentWord = "";
+	var typedWord = "";
+	var wordCount = 0;
 	
 	var lines = [];
 	var lineCount = 0;
@@ -53,20 +32,22 @@ $(document).ready(function(){
 		console.log(letters);
 		var line = $('<div class="letters"></div>');
 		$(letters).each(function() {
-		var letter = this;
-		if(letter == ' ') {
-			letter = '&nbsp;';
-		}
-		line.append($('<div class="letter"></div>').html(letter));
+			var letter = this;
+			if(letter == ' ') {
+				letter = '&nbsp;';
+			}
+			line.append($('<div class="letter"></div>').html(letter));
+		});
+		lines.push(line);
 	});
-	lines.push(line);
-});
+
 
 	$("#keyboard-upper-container").hide();
 	$("#keyboard-lower-container").show();
 	$("#words-typed").html("");
 	$("#words").html(lines[lineCount]);
 	$(".letter").eq(0).addClass("highlight-red");
+	$("#next-letter").html($(".letter").eq(0).html());
 
 	$(document).keydown(function(event){
 		if (event.which === 16) {
@@ -75,6 +56,7 @@ $(document).ready(function(){
         }
 //        $("#words-typed").html("Key: " + event.which + " keycode: " + event.keyCode);
     });
+
 	$(document).keyup(function(event){
 		if (event.which === 16) {
 			$("#keyboard-upper-container").hide();
@@ -82,48 +64,84 @@ $(document).ready(function(){
         }
 //        $("#words-typed").html("Key: " + event.which + " keycode: " + event.keyCode);
     });	
-	$(document).keypress(function(event){
-		var key_char;
-		var key_id;
-		$("#words-typed").append(String.fromCharCode(event.which));
-		if (((event.which > 47) && (event.which < 58)) || ((event.which > 64) && (event.which < 91)) || ((event.which > 96) && (event.which < 123))) {
-			key_char = String.fromCharCode(event.which);
-			key_id = "#" + key_char;
-//			console.log(event.which.toString());
-//			console.log(String.fromCharCode(event.which));
-		} else {
-			key_id = "#" + event.which.toString();
-//			console.log("non letter pressed");
-		}
-//		console.log(key_id.toString());
-//		$(key_id).html("PR");
-		var keyBGColor = $(key_id).css("background-color");
-		var keyFGColor = $(key_id).css("color");
-		console.log(keyBGColor);
-		console.log(keyFGColor);
-		$(key_id).css('background-color', '#8888ff');
-		$(key_id).css('color', '#ffffff');
-//		$(key_id).animate({width: '-=30px'},1000);
-		$(key_id).animate({
-			backgroundColor: 'rgb(245,245,245)',
-			color: 'rgb(51,51,51)'
-		},100);
-		
-		console.log($(".letter").eq(letterCount).html());
-		$(".letter").removeClass("highlight-red");
-		$(".letter").eq(letterCount).addClass("highlight-red");
-		$(".letter").eq(letterCount).removeClass("highlight-red");
-		$(".letter").eq(letterCount+1).addClass("highlight-red");
-		if ($(".letter").eq(letterCount).html() == key_char) {
-			$(".letter").eq(letterCount).addClass("highlight");
-		}
-		console.log(letterCount);
-		letterCount++;
-//		while (!done) {
-//			if (newLineStarted) {
-//				if ($(".letter").html[letterCount] == 
-//			}			
-//		}		
-    });
 
+	$(document).keypress(function(event){
+		if (!bStarted) {
+			bStarted = true;
+			start_time = performance.now();
+//			console.log(start_time);
+		}
+
+		if (!bDone) {
+			$("#words-typed").append(String.fromCharCode(event.which));
+			if (((event.which > 47) && (event.which < 58)) || ((event.which > 64) && (event.which < 91)) || ((event.which > 96) && (event.which < 123))) {
+				key_char = String.fromCharCode(event.which);
+				key_id = "#" + key_char;
+			} else {
+				key_id = "#" + event.which.toString();
+				if (event.which.toString() == 32) key_char = '&nbsp;';
+			}
+			$(key_id).css('background-color', '#8888ff');
+			$(key_id).css('color', '#ffffff');
+			$(key_id).animate({
+				backgroundColor: 'rgb(245,245,245)',
+				color: 'rgb(51,51,51)'
+			},100);
+		
+			currentLetter = $(".letter").eq(letterCount).html();
+			$(".letter").removeClass("highlight-red");
+			$(".letter").eq(letterCount).addClass("highlight-red");
+			$(".letter").eq(letterCount).removeClass("highlight-red");
+			$(".letter").eq(letterCount+1).addClass("highlight-red");
+			$("#next-letter").html($(".letter").eq(letterCount+1).html());
+			if (currentLetter == '&nbsp;') {
+				if (currentWord == typedWord) wordCount++;
+				currentWord = "";
+				typedWord = "";
+			} else {
+				currentWord += currentLetter;
+				typedWord += key_char;
+			}
+			if (currentLetter == key_char) {
+				$(".letter").eq(letterCount).addClass("highlight");
+				$("#icons").html($("#icons").html() + '<img src="check.jpeg" />');
+			} else {
+				mistakeCount++;
+				$("#icons").html($("#icons").html() + '<img src="x.jpeg" />');
+			}
+			letterCount++;
+			if (letterCount == $(".letter").length) {
+				lineCount++;
+				currentWord += currentLetter;
+				typedWord += key_char;
+				if (currentWord == typedWord) {
+					wordCount++;
+					currentWord = "";
+					typedWord = "";
+				}
+				if (lineCount == lines.length) {
+					bDone = true;
+					stop_time = performance.now();
+					DisplayResults();	
+				} else {
+					letterCount = 0;
+					$("#words-typed").html("");
+					$("#words").html(lines[lineCount]);
+					$(".letter").eq(0).addClass("highlight-red");
+					$("#next-letter").html($(".letter").eq(0).html());
+					$("#icons").html("");
+				}
+			}
+		}
+    });
+	
+	function DisplayResults() {
+		var time_minutes = ((stop_time - start_time ) / 60000).toFixed(1);
+		var wpm = ((wordCount / time_minutes) - (mistakeCount * 2)).toFixed(1);
+		$("#words-typed").html("");
+		$("#icons").html("");
+		$("#next-letter").html("");
+		$("#words").html("<div>Words correctly typed: " + wordCount.toString() + " Mistakes: " + mistakeCount.toString() + " Words per minute: " + wpm.toString());
+	}
+	
 });
